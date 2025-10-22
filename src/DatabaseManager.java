ae75c928b699b86d3cb411a4a363d85c20960c06
Samub96 <samuel.d.barona@gmail.com>
2025-10-21 21:18:08 -0500

    finishing cibersecurity project pending documentar

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -1,11 +1,9 @@
-
-
 import java.sql.*;
 
 public class DatabaseManager {
-    private static final String URL = "jdbc:mysql://localhost:3306/login_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "tu_password";
+    private static final String URL = "jdbc:mysql://localhost:3306/DB?serverTimezone=UTC";
+    private static final String USER = "dbTest";
+    private static final String PASSWORD = "password";
 
     static {
         try {
